7d0a3d250fa8cdf2cc24868f7fea9bdbc238f1d7
Naresh <nareshsu427@gmail.com>
2024-07-17 21:40:12 +0530

    add prottectd route

--- src/layouts/Dashborad.tsx
+++ src/layouts/Dashborad.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuthState } from "../store";
 
 const Dashborad = () => {
+  const { user } = useAuthState();
+  if (user === null) {
+    return <Navigate to="/auth/login" replace={true} />;
+  }
   return (
     <div>
       Dashborad Components
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthState } from "../store";
 
 const NonAuth = () => {
+  const { user } = useAuthState();
+  if (user !== null) {
+    return <Navigate to="/" replace={true} />;
+  }
   return (
     <div>
       Non Auth Componets
